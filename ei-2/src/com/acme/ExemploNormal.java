c90e253d6d5332cbbe8775b8bf276fbd604f9e15
Isadora Beckmann <beckmannisadora@gmail.com>
2026-03-08 18:10:11 -0300

    estudo independente 2: text block

---
+++ ei-2/src/com/acme/ExemploNormal.java
@@ -0,0 +1,18 @@
+package com.acme;
+
+public class ExemploNormal {
+    public static void main(String[] args) {
+
+       String texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+               "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n" +
+               "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris\n" +
+               "nisi ut aliquip ex ea commodo consequat.\n" +
+               "Duis aute irure dolor in reprehenderit in voluptate velit esse\n" +
+               "cillum dolore eu fugiat nulla pariatur.\n" +
+               "Excepteur sint occaecat cupidatat non proident,\n" +
+               "sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+        System.out.println(texto);
+    }
+    
+}
