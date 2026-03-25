c3d70ad77f8603429ba0799a2280a0d23f24d0df
Isadora Beckmann <beckmannisadora@gmail.com>
2026-03-24 21:44:13 -0300

    unidade 2: serialize retorno

--- unidade-2/src/com/acme/TestarArquivo.java
+++ unidade-2/src/com/acme/TestarArquivo.java
@@ -1,15 +1,18 @@
 package com.acme;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TestarArquivo {
 
     public static void main(String[] args) {
-        Usuario u1 = new Usuario("Maria", "123");
+        /*Usuario u1 = new Usuario("Maria", "123");
         Usuario u2 = new Usuario("Joao", "456");
         Usuario u3 = new Usuario("Jose", "789");
         
@@ -18,18 +21,40 @@ public class TestarArquivo {
         usuarios.add(u2);
         usuarios.add(u3);
         
-        persistir("usuarios.dat", usuarios);
+        persistir("usuarios.dat", usuarios);*/
+        
+        List<Usuario> usuarios = carregar("usuarios.dat");
+        
+        usuarios.forEach(usuario -> System.out.println(usuario.getNome() + ":" + usuario.getDataCriacao()));
     }
     
     private static void persistir(String arquivo, ArrayList<Usuario> usuarios) {
-        try {
-            FileOutputStream fos = new FileOutputStream(arquivo);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(usuarios);
+        try (FileOutputStream fos = new FileOutputStream(arquivo)){
+            try (ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+                oos.writeObject(usuarios);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+    
+    private static List<Usuario> carregar(String arquivo) {
+        try (FileInputStream fis = new FileInputStream(arquivo)) {
+            try (ObjectInputStream ois = new ObjectInputStream(fis)) {
+                return (List<Usuario>) ois.readObject();
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        return null;
     }
 }
