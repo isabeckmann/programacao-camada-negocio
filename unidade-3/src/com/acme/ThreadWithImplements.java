658e90f33955716c5777b737d99bf4d4898f863a
Isadora Beckmann <beckmannisadora@gmail.com>
2026-06-02 21:55:29 -0300

    unidade 3: multi-thread

---
+++ unidade-3/src/com/acme/ThreadWithImplements.java
@@ -0,0 +1,41 @@
+package com.acme;
+
+class App {
+    
+}
+
+class Printer extends App implements Runnable {
+    private int copies;
+    private String doc;
+    
+    public Printer(int copies, String doc) {
+        this.copies = copies;
+        this.doc = doc;
+    }
+    
+    @Override
+    public void run() {
+        System.out.println("Thread rodando");
+        for (int i=0; i<copies; i++) {
+            System.out.println("Printing doc "+doc+" copy #"+i);  
+        }
+    }
+}
+
+public class ThreadWithImplements {
+    public void main(String[] args) {
+        System.out.println("Iniciando método main...");
+        
+        Printer p1 = new Printer(5, "myProfile.txt");
+        Thread t1 = new Thread(p1);
+        t1.start();
+        
+        System.out.println("Going to thread 2...");
+        
+        Printer p2 = new Printer(10, "researchProject.pdf");
+        Thread t2 = new Thread(p2);
+        t2.start();
+        
+        System.out.println("Finalizando aplicação");
+    }
+}
