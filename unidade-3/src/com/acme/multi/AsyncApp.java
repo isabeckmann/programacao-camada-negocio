dab3d64bb2eeeed47675650cfe54b02a7e952e8d
Isadora Beckmann <beckmannisadora@gmail.com>
2026-06-09 21:10:59 -0300

    unidade 3: synchronizedList

--- unidade-3/src/com/acme/multi/AsyncApp.java
+++ unidade-3/src/com/acme/multi/AsyncApp.java
@@ -4,35 +4,67 @@ import java.util.*;
 import java.lang.Runnable;
 
 public class AsyncApp {
-    private static final List<String> list = new ArrayList();
+    private static final List<String> list;
+    private static final int QUANTIDADE = 100;
+    
+    static {
+        //list = new ArrayList();
+        //quando usa essa synchronizedList não precisa usar o dentro do for
+        list = Collections.synchronizedList(new ArrayList());
+    }
     
     public static void main(String[] args) {
         Thread t1 = new Thread( new Runnable(){
         
             @Override
             public void run() {
-                list.add("A");
+                for (int i=0; i<QUANTIDADE; i++) {
+                    //synchronized (list) {
+                        list.add("A");
+                    //}
+                }
             }
         });
-        t1.start();
         
         Thread t2 = new Thread( new Runnable(){
         
             @Override
             public void run() {
-                list.add("B");
+                for (int i=0; i<QUANTIDADE; i++) {
+                    //synchronized (list) {
+                        list.add("B");
+                    //}
+                }
             }
         });
-        t2.start();
         
         Thread t3 = new Thread( new Runnable(){
         
             @Override
             public void run() {
-                list.add("C");
+                for (int i=0; i<QUANTIDADE; i++) {
+                    //synchronized (list) {
+                        list.add("C");
+                    //}
+                }
             }
         });
+        
+        t1.start();
+        t2.start();
         t3.start();
+        
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException interruptedException) {
+        }
+        
+        for(String s: list) {
+            System.out.println(s);
+        }
+        System.out.println("Size --> "+list.size());
     }
     
 }
